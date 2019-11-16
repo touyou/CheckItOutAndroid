d9254a327ca508bf80aab8d93070ca3dc23ce467
touyou <fujiyou1121@gmail.com>
2019-11-16 23:43:34 +0900

    layout pad view

--- app/src/main/java/dev/touyou/checkitoutandroid/ui/main/MainFragment.kt
+++ app/src/main/java/dev/touyou/checkitoutandroid/ui/main/MainFragment.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import dev.touyou.checkitoutandroid.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -15,6 +17,12 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private val pads by lazy {
+        listOf(pad1, pad2, pad3, pad4,
+            pad5, pad6, pad7, pad8,
+            pad9, pad10, pad11, pad12,
+            pad13, pad14, pad15, pad16)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +34,41 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        for ((index, pad) in pads.withIndex()) {
+            pad.setOnTouchListener { _, motionEvent ->
+                when(motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> pad.setImageResource(selectedPadImage(index))
+                    MotionEvent.ACTION_UP -> pad.setImageResource(padImage(index))
+                    else -> {}
+                }
+                true
+            }
+        }
+    }
+
+
+    private fun selectedPadImage(index: Int): Int {
+        return when(index) {
+            in 0..3 -> R.drawable.selected_red_pad
+            in 4..7 -> R.drawable.selected_yellow_pad
+            in 8..11 -> R.drawable.selected_green_pad
+            in 12..15 -> R.drawable.selected_blue_pad
+            else -> R.drawable.selected_red_pad
+        }
+    }
+
+    private fun padImage(index: Int): Int {
+        return when(index) {
+            in 0..3 -> R.drawable.red_pad
+            in 4..7 -> R.drawable.yellow_pad
+            in 8..11 -> R.drawable.green_pad
+            in 12..15 -> R.drawable.blue_pad
+            else -> R.drawable.red_pad
+        }
+    }
 }
